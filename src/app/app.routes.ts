f272cae719058629d530321f5424843631cbe293
Owen <37266971+Sortient@users.noreply.github.com>
2025-03-02 17:53:32 +0000

    12 update home page styling (#13)
    * Updated styling across site
    
    * Updated workflow to trigger preview build on pull request
    
    * Updated unit tests to run and added build as a dependency to the vercel deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    //{ path: 'about', component: AboutComponent },
+    { path: 'about', component: AboutComponent },
     { path: 'portfolio', component: PortfolioComponent },
     //{ path: 'contact', component: ContactComponent },
     // { path: '', redirectTo: '/home', pathMatch: 'full' },
